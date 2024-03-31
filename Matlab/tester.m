6136341b137c0eb858c671e95a6d4c96473e4d81
Evgeny Mirkes <em322@le.ac.uk>
2024-03-31 13:01:18 +0100

    Tester changed to QSAR dataset.

--- Matlab/tester.m
+++ Matlab/tester.m
@@ -1,6 +1,17 @@
 % tester
-%% Load data for work
-load('dataTr.mat')
+% For all tests the QSAR database is used
+% Mansouri, K.; Ringsted, T.; Ballabio, D.; Todeschini, R.; Consonni, V.
+% Quantitative structure–activity relationship models for ready
+% biodegradability of chemicals. J. Chem. Inf. Model. 2013, 53, 867–878.  
+% QSAR biodegradation. UCI Machine Learning Repository. Available online:
+% https://archive.ics.uci.edu/ml/datasets/QSAR+biodegradation (accessed on
+% 31 March 2024).  
+
+%% load data and split them to two clases
+tab = readtable("qsar.csv");
+data = table2array( tab(:, 1:41));
+outcome = table2array( tab(:, 42));
+
 %% test of oneDClass and Fisher
 % Index of positive (bad) outcome
 ind = outcome == 1;
@@ -19,10 +30,6 @@ for k = 1:5
     [bestT, bestErr, dir1] = specDir(x, y, ddir, {"Random score for Binary GOSE outcome", "Bad", "Good"}, 'ba');
     saveFigures(['figures/Random_', num2str(k), '.png']);
 end
-
-%% Comparison of direction usage
-[bestT, bestErr, dir] = fisher(x, y, {"LFD score for Binary GOSE outcome", "Bad", "Good"}, 'ba');
-[bestT, bestErr, dir1] = specDir(x, y, -dir, {"Random score for Binary GOSE outcome", "Bad", "Good"}, 'ba');
 
 %% Test of function max search methods.
 nRep = 10000;
@@ -57,10 +64,15 @@ end
 %     fprintf("Delta %f Ns %d rho %f time %f\n", Da(K), Ns(K), rho, a);
 % end
 
-
-
-%% test of final function
-load('dataSplit.mat');
+%% Split data to test and training sets
+ind = randsample(size(x, 1), floor(0.2 * size(x, 1)));
+xTe = x(ind, :);
+x(ind, :) = [];
+xTr = x;
+ind = randsample(size(y, 1), floor(0.2 * size(y, 1)));
+yTe = y(ind, :);
+y(ind, :) = [];
+yTr = y;
 
 % Form model for training set
 [bestT, bestErr, dir] = fisher(xTr, yTr, [], 'ba');
@@ -86,9 +98,13 @@ pred = [predX; predY];
 %
 lab = lab+1;
 pred = pred+1;
-%% Test itself
+
+% Test itself
 mdl = modelCreator(data, lab, pred);
 
 %% Test of estimate
 [gen, det] = estimate(mdl);
+
+%% Test with individual estimations
+[gen, det] = estimate(mdl, 'Auto', data, lab);
 
